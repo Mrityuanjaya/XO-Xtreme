cf7ec537cf2db9bee62a9ad0335ac150ea186bec
Mrityuanjaya <mrityuanjaya.gupta@joshtechnologygroup.com>
2024-05-07 00:41:10 +0530

    Added responsive breakpoints for small devices

--- src/components/BigSquare/BigSquare.jsx
+++ src/components/BigSquare/BigSquare.jsx
@@ -9,7 +9,7 @@ const BigSquare = (props) => {
         <div className="border-4 border-gray-600 p-2 md:p-4 md:w-52 md:h-52 w-28 h-28 flex items-center justify-center">
             {contextObj.bigSquares[boardIdx] ? (
                 <h2
-                    className={`text-9xl ${
+                    className={`md:text-9xl text-5xl ${
                         contextObj.bigSquaresColors[boardIdx] === "red"
                             ? "text-red-400"
                             : "black"
