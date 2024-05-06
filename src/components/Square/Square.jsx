172f74e12a52f9f7bbd3d44ac80cf301e94ea1bf
Mrityuanjaya <mrityuanjaya.gupta@joshtechnologygroup.com>
2024-05-07 00:36:13 +0530

    Added responsive breakpoints for small devices

--- src/components/Square/Square.jsx
+++ src/components/Square/Square.jsx
@@ -1,7 +1,7 @@
 const Square = (props) => {
     return (
         <button
-            className={`w-1/3 h-1/3 text-3xl font-bold border-4 ${
+            className={`w-1/3 h-1/3 text-lg md:text-3xl font-bold border-2 md:border-4 ${
                 props.isActive ? "border-blue-300" : "border-gray-300"
             }`}
             onClick={props.onSquareClick}
